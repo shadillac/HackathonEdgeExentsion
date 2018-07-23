38eaa1aceb17543235d4560efd060e0b8289f851
feralmonkey <feralmonkey@users.noreply.github.com>
2018-07-23 13:02:31 -0700

    Updates

--- js/popup.js
+++ js/popup.js
@@ -6,30 +6,7 @@ let responseLabel = document.getElementById('responseLabel');
 go.onclick = function() {
   let apiURI = document.getElementById('apiUri').value;
   let methodType = document.getElementById('methodBox').value; 
-<<<<<<< HEAD
-
-  // alert(apiURI);
-
-  // const response = fetch(apiURI)
-  // .then(function(response){
-  //   alert(response.json());
-  //   return response.json();
-  // })
-  // .then(function(jsonData){
-  //   alert('sup dog');
-  // })
-  // .catch(function(error){
-  //   alert(error);
-  // });
-
-  //const json = await response.json;
-  //responseLabel.value = json;
-  
-  // alert("ending");
-
-=======
 alert(apiURI);
->>>>>>> 5c8da93175a71dab94133b8bf0ede07b9a550047
   fetch(apiURI, {
     method: 'GET',
     headers: {
@@ -37,7 +14,6 @@ alert(apiURI);
     },
   })
   .then(function (response) {
-<<<<<<< HEAD
     return response.text();
   })
   .then(function (text) {
@@ -46,12 +22,4 @@ alert(apiURI);
   .catch(function(error){
     alert(error);
   });  
-
-=======
-    responseLabel.value = response.text();
-  })
-  .then(function (text) {
-    responseLabel.value = text();
-  });
->>>>>>> 5c8da93175a71dab94133b8bf0ede07b9a550047
 };
